6bcd821fa8d4e0cd6906f739b8f1b2a5649b0072
zaidanrafif <121496244+zaidanrafif@users.noreply.github.com>
2023-05-02 01:51:29 +0700

    TP_MOD10_1302213072

---
+++ AljabarLibraries/PersamaanKuadrat.cs
@@ -0,0 +1,42 @@
+﻿namespace AljabarLibraries
+{
+    public static class PersamaanKuadrat
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double D = b * b - 4 * a * c;
+
+            if (D < 0)
+            {
+                throw new Exception("Persamaan ini tidak memiliki akar real");
+            }
+            else if (D == 0)
+            {
+                double x = -b / (2 * a);
+                return new double[] { x };
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                return new double[] { x1, x2 };
+            }
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double hasilKuadratA = a * a;
+            double hasilKuadratB = 2 * a * b;
+            double hasilKuadratC = b * b;
+
+            return new double[] { hasilKuadratA, hasilKuadratB, hasilKuadratC };
+        }
+    }
+}
---
+++ ShowCase/Program.cs
@@ -0,0 +1,52 @@
+﻿using AljabarLibraries;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("A. Mencari akar - akar dari persamaan kuadrat");
+        Console.Write("Masukkan koefisien a, b, c: ");
+        string[] inputKoefisien = Console.ReadLine().Split(',');
+        double a = Convert.ToDouble(inputKoefisien[0]);
+        double b = Convert.ToDouble(inputKoefisien[1]);
+        double c = Convert.ToDouble(inputKoefisien[2]);
+
+        double[] persamaan = { a, b, c };
+        try
+        {
+            double[] akar = PersamaanKuadrat.AkarPersamaanKuadrat(persamaan);
+            Console.Write("Output: {");
+            for (int i = 0; i < akar.Length; i++)
+            {
+                Console.Write(akar[i]);
+                if (i < akar.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }Console.WriteLine("}");Console.WriteLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        Console.WriteLine("B. Mendapatkan hasil kuadrat dari persamaan berpangkat 1");
+        Console.Write("Masukkan koefisien a, b: ");
+        inputKoefisien = Console.ReadLine().Split(',');
+        a = Convert.ToDouble(inputKoefisien[0]);
+        b = Convert.ToDouble(inputKoefisien[1]);
+
+        double[] persamaan2 = { a, b };
+        double[] hasilKuadrat = PersamaanKuadrat.HasilKuadrat(persamaan2);
+        Console.Write("Output: {");
+        for (int i = 0; i < hasilKuadrat.Length; i++)
+        {
+            Console.Write(hasilKuadrat[i]);
+            if (i < hasilKuadrat.Length - 1)
+            {
+                Console.Write(", ");
+            }
+            
+        }Console.WriteLine("}");
+    }
+}
